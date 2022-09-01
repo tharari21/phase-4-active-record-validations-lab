ab20efcba53e84347b1d0e76fde1f048cad3fe3b
Tomer Harari <tharari21@gmail.com>
2022-09-01 10:06:52 -0400

    commit tree 74a90565316477a902291d40c269885b3f6619c8

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, {:length => {:minimum => 250}}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: ['Fiction', 'Non-Fiction']
+    validate :clickbait?
+
+    CLICKBAIT_PATTERNS = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top \d/i,
+        /Guess/i
+    ]
+    def clickbait?
+        if CLICKBAIT_PATTERNS.none? { |pattern| pattern.match(title)}
+            errors.add(:title, "Must be clickbait")
+        end
+
+    end
+    
 end
